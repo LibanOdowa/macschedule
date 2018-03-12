97fa5826712f49828311c14e3a567b65e48c71ca
Liban <lodowa@macalester.edu>
2018-03-12 18:17:04 -0500

    Implemented json file, still needs to be cleaned up

--- main.py
+++ main.py
@@ -3,18 +3,31 @@ import json
 
 soup = BeautifulSoup(open("raw.html", encoding="utf8"), "html.parser")
 
-
-courseTitle = soup.find_all("td", {"class": "class-schedule-course-title"})
 courseNumber = soup.select(".class-schedule-course-number")
+courseTitle = soup.select(".class-schedule-course-title")
+courseData = soup.select(".class-schedule-label")
+
 
 file = open("data.json", "w")
 data = []
-for i in range(len(courseTitle) - 1):
-    title = courseTitle[i].getText()
+
+#Dirty data right now, not formatted well times/dates/classes
+for i in range(len(courseTitle)):
     number = courseNumber[i].getText()
+    title = courseTitle[i].getText()
+    days = courseData[i*5].getText()
+    time = courseData[i*5+1].getText()
+    room = courseData[i*5+2].getText()
+    instructor = courseData[i*5+3].getText()
+    enrollment = courseData[i*5+4].getText()
     data.append({
+        "number": number,
         "title": title,
-        "number": number
+        "days": days,
+        "time": time,
+        "room": room,
+        "instructor": instructor,
+        "enrollment": enrollment
     })
 json.dump(data,file)
 file.close()
