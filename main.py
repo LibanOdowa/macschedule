48f75304f978ec357215d42b0d954c79d2918a21
Liban <lodowa@macalester.edu>
2018-03-12 17:55:03 -0500

    Basic scraping and json output

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello world")
+from bs4 import BeautifulSoup
+import json
+
+soup = BeautifulSoup(open("raw.html", encoding="utf8"), "html.parser")
+
+
+courseTitle = soup.find_all("td", {"class": "class-schedule-course-title"})
+courseNumber = soup.select(".class-schedule-course-number")
+
+file = open("data.json", "w")
+data = []
+for i in range(len(courseTitle) - 1):
+    title = courseTitle[i].getText()
+    number = courseNumber[i].getText()
+    data.append({
+        "title": title,
+        "number": number
+    })
+json.dump(data,file)
+file.close()
---
+++ pagegrabber.py
@@ -0,0 +1,7 @@
+import requests
+res = requests.get('https://www.macalester.edu/registrar/schedules/2018spring/class-schedule/')
+res.raise_for_status()
+outputFile = open('raw.html', 'wb')
+for chunk in res.iter_content(100000):
+    outputFile.write(chunk)
+outputFile.close()
