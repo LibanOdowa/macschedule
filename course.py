3690ef43e0762355c6fe68bc4746c9f5abe93f48
Liban <lodowa@macalester.edu>
2018-03-13 16:12:25 -0500

    Cleaned up everything except rooms for inputs

--- course.py
+++ course.py
@@ -1,13 +1,33 @@
+from datetime import datetime
+
 class Course:
     def __init__(self, number, title, days, time, room, instructor, enrollment):
-        #Inputs dirty data, clean this up before dumping
-        self.number = number
+        #TODO: Clean up data for usage
+
+        self.department = number.split()[0]
+        self.number = number.split()[1].split('-')[0]
+        self.section = number.split()[1][-2:]
         self.title = title
-        self.days = days
-        self.time = time
-        self.room = room
-        self.instructor = instructor
-        self.enrollment = enrollment
+        self.days = days.split()[1]
+        if "TBA" in time:
+            self.startTime = "TBA"
+            self.endTime = "TBA"
+        else:
+            # self.startTime = datetime.strptime(time[6:14],'%H:%M %p').strftime('%H:%M %p')
+            # self.endTime = datetime.strptime(time[15:],'%H:%M %p').strftime('%H:%M %p')
+            self.startTime = datetime.strptime(time[6:14],'%H:%M %p')
+            self.endTime = datetime.strptime(time[15:],'%H:%M %p')
+        self.room = ' '.join(room.split()[1:])
+        self.instructor = ' '.join(instructor.split()[1:])
+
+
+        #TODO: WORK ON ENROLLMENT FORMATTING "CLOSED" IS AN ISSUE
+        # self.currentEnrollment = enrollment.split()[1]
+        # if enrollment.split()[3] == '/':
+        #     self.maxEnrollment = enrollment.split()[4]
+        # else:
+        #     self.maxEnrollment = enrollment.split()[3]
+
 
     def output(self):
         print(self.__dict__)
